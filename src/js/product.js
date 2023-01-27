4d694c967c0bc5fa84049ffb7e0aee40695caa23
timthayne <timthayne@gmail.com>
2023-01-27 09:45:39 -0700

    Test for demo

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,23 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let products = getLocalStorage('so-cart');
+  
+  // check to see if products exist
+  if (products) {
+    products.push(product);
+    
+  } else {
+    products = [product];
+  }
+
+  // add product array to local storage
+  setLocalStorage('so-cart', products); 
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
