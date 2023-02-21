c20e632754f2412839011a8c47c508b7221947db
timthayne <timthayne@gmail.com>
2023-02-21 12:36:57 -0700

    Completed Team 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,9 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
+const productId = getParam('product');
 
-function addProductToCart(product) {
-  let products = getLocalStorage('so-cart');
-
-  // check to see if products exist
-  if (products) {
-    products.push(product);
-  } else {
-    products = [product];
-  }
-
-  // add product array to local storage
-  setLocalStorage('so-cart', products);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
