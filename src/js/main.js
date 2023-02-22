97c1b6b5b6d2d82482da69d45d42baf841b4461c
timthayne <timthayne@gmail.com>
2023-02-22 10:45:47 -0700

    Fixed errors for Team 3

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,6 @@ import ProductList from './ProductList.mjs';
 
 const dataSource = new ProductData('tents');
 const element = document.querySelector('.product-list');
-const listing = new ProductList('Tents', dataSource, element);
+const listing = new ProductList('tents', dataSource, element);
 
 listing.init();
